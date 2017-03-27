89239bacbf916572ee188bfc4f25df2c668401ac
krzys1ek <combat100@gmail.com>
2017-03-27 10:37:58 +0200

    amdesign

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var sourcemaps  = require('gulp-sourcemaps');
+
+// Static Server + watching scss/php files
+gulp.task('serve', ['sass'], function() {
+    
+    browserSync.init({
+        proxy: "http://localhost/projekt/"
+    });
+    
+    gulp.watch("sass/*.scss", ['sass']);
+    gulp.watch("*.php").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("sass/*.scss")
+        .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
