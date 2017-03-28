d8bfe6f9cb731d74fc63f39b2bb9bf31544ff61e
krzys1ek <combat100@gmail.com>
2017-03-28 15:23:25 +0200

    slider

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-$zmienna = 'dziala';
 
-console.log($zmienna);
+
+
+$(function() {
+    $('.slider').vegas({
+        delay: 8000,
+        slides: [
+            { src: 'images/1.png' },
+            { src: 'images/2.png' },
+            { src: 'images/3.png' },
+            { src: 'images/4.png' }
+        ],
+                animation: 'kenburns',
+                        
+
+
+//            overlay: 'bower_components/vegas/dist/overlays/02.png',
+
+      
+    
+    });
+});
