09f527fe22a39c1ce84352c0b6c7bf8f464d7ba0
krzys1ek <combat100@gmail.com>
2017-03-30 22:54:46 +0200

    correct

--- js/app.js
+++ js/app.js
@@ -1,29 +1,56 @@
 $(function() {
     $('.slider').vegas({
         delay: 8000,
-        slides: [
-            { src: 'images/1.png' },
-            { src: 'images/2.png' },
-            { src: 'images/3.png' },
-            { src: 'images/8.png' },
-            { src: 'images/6.png' }
+        slides: [{
+                src: 'images/4.jpg'
+            },
+            {
+                src: 'images/2.jpg'
+            },
+            {
+                src: 'images/3.jpg'
+            },
+            {
+                src: 'images/1.jpg'
+            },
+            {
+                src: 'images/8.jpg'
+            }
         ],
-                animation: 'kenburns',
-
-
-    
+        animation: 'kenburns',
     });
 });
 
+$("#contact-form").validate({
+ errorClass: "text-error",
+rules: {
+subject: "required",
+message: "required",
+firstName: "required",
+email: {
+  required: true,
+  email: true
+}
+},
+messages: {
+subject: "Proszę podać swoje Nazwisko",
+message: "Nie wpisałeś treści wiadomości",
+firstName:"podaj swoję Imię",
+email: {
+  required: "Nie podałeś swojego adresu e-mail",
+  email: "Błędny format adresu e-mail"
+}
+}
+});
+
 $('.projects').hover(function() {
-         $('ul.margin').css('display', 'block');
-     }, function() {
-         $('ul.margin').css('display', 'none');
-     });
+    $('ul.margin').css('display', 'block');
+}, function() {
+    $('ul.margin').css('display', 'none');
+});
 
 
-
-    lightbox.option({
-      'resizeDuration': 200,
-      'wrapAround': true,
-    });
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+});
